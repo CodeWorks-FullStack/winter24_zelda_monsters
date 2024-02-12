6df3382dededac0a20f2de1a1b69d49e6cb22268
Jeremy Fowler <jembyfowler@gmail.com>
2024-02-12 11:15:37 -0700

    made request, getting promise back from axios

--- app/controllers/MonstersController.js
+++ app/controllers/MonstersController.js
@@ -3,6 +3,7 @@ import { monstersService } from "../services/MonstersService.js";
 export class MonstersController {
   constructor () {
     console.log('monsters controller loaded');
+    this.getMonsters()
   }
 
   getMonsters() {
