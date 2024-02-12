8dd329326aee5a8584f73830889990fa46bae3cc
Jeremy Fowler <jembyfowler@gmail.com>
2024-02-12 11:47:15 -0700

    promise testing

--- app/controllers/MonstersController.js
+++ app/controllers/MonstersController.js
@@ -4,9 +4,25 @@ export class MonstersController {
   constructor () {
     console.log('monsters controller loaded');
     this.getMonsters()
+
+    console.log('Running 1st');
+    console.log('Running 2nd');
+    this.waitForPromise()
+    console.log('Running 4th');
+
   }
 
   getMonsters() {
-    monstersService.getMonsters()
+    // monstersService.getMonsters()
+  }
+
+  async waitForPromise() {
+    const message = await new Promise((resolve, reject) => {
+      setTimeout(() => {
+        resolve('running 3rd')
+      }, 3000,)
+    })
+
+    console.log(message);
   }
 }
--- app/services/MonstersService.js
+++ app/services/MonstersService.js
@@ -4,6 +4,7 @@ class MonstersService {
   getMonsters() {
     const response = zeldaApi.get('category/monsters')
     console.log('📡 got monsters from api', response);
+    // console.log('📡looking at data', response.data.data[0]);
   }
 }
 
